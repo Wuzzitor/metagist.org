5528936fcb67b1a28e815875f43701d0c66009c8
Daniel Pozzi <bonndan76@googlemail.com>
2013-05-05 20:58:04 +0200

    corrected the callback method in prod.dist

--- config/prod.dist.php
+++ config/prod.dist.php
@@ -21,7 +21,7 @@ $app['db.options'] = array(
  */
 $app["opauth"] = array(
     "login" => "/auth/login",
-    "callback" => "/auth/callback",
+    "callback" => "/auth/login/github/oauth2callback",
     "config" => array(
         "security_salt" => "",
         "Strategy" =>  array(
