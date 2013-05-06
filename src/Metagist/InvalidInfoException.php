8ecdbf695992a0ca6bf1b133d3157e32b082ebe1
Daniel Pozzi <bonndan76@googlemail.com>
2013-05-06 20:45:52 +0200

    Invalid info exception

---
+++ src/Metagist/InvalidInfoException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Metagist;
+
+/**
+ * Exception which is thrown if a metainfo is not valid.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class InvalidInfoException extends Exception
+{
+    
+}
