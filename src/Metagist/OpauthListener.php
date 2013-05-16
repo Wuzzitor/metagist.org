efce24e2473985577e1d8b86fb4e7cb32233dd0d
Daniel Pozzi <bonndan76@googlemail.com>
2013-05-16 22:55:39 +0200

    opauth listener uses the logger if available

--- src/Metagist/OpauthListener.php
+++ src/Metagist/OpauthListener.php
@@ -9,11 +9,14 @@ use \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManage
 use \Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use \Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use \Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use \Psr\Log\LoggerInterface;
 
 /**
+ * Listener for successful opauth authentication.
  * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
  */
-class OpauthListener implements EventSubscriberInterface, ListenerInterface
+class OpauthListener implements EventSubscriberInterface //, ListenerInterface
 {
     /**
      * context
@@ -21,24 +24,41 @@ class OpauthListener implements EventSubscriberInterface, ListenerInterface
      */
     private $context;
     
+    /**
+     * auth manager
+     * @var \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager
+     */
     private $manager;
     
+    /**
+     * user provider
+     * @var UserProvider
+     */
     private $provider;
+    
+    /**
+     * logger
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
     
     /**
      * Constructor.
      * 
      * @param \Symfony\Component\Security\Core\SecurityContext $context
      * @param \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager $manager
+     * @param UserProvider $provider
      */
     public function __construct(
         SecurityContext $context,
         AuthenticationProviderManager $manager,
-        UserProvider $provider
+        UserProvider $provider,
+        LoggerInterface $logger = null
     ) {
         $this->context  = $context;
         $this->manager  = $manager;
         $this->provider = $provider;
+        $this->logger   = $logger;
     }
     
     /**
@@ -55,6 +75,13 @@ class OpauthListener implements EventSubscriberInterface, ListenerInterface
         $token = new PreAuthenticatedToken($user, '', 'opauth');
         $this->manager->authenticate($token);
         $this->context->setToken($token);
+        $this->log("GitHub auth success:" . var_export($user, true));
+    }
+    
+    public function onError(GenericEvent $event)
+    {
+        $response = $event->getSubject();
+        $this->log(var_export($response, true));
     }
     
     /**
@@ -65,13 +92,26 @@ class OpauthListener implements EventSubscriberInterface, ListenerInterface
     public static function getSubscribedEvents()
     {
         return array(
-            \SilexOpauth\OpauthExtension::EVENT_SUCCESS => 'onSuccess'
+            \SilexOpauth\OpauthExtension::EVENT_SUCCESS => 'onSuccess',
+            \SilexOpauth\OpauthExtension::EVENT_ERROR => 'onError'
         );
     }
 
     public function handle(GetResponseEvent $event)
     {
-        
+        $event;
+    }
+
+    /**
+     * info-logs a message.
+     * 
+     * @param string $message
+     */
+    protected function log($message)
+    {
+        if ($this->logger) {
+            $this->logger->info($message);
+        }
     }
 
 }
