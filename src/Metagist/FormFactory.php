e12674a37d647c4fc9953a7c5c6c4b5d69c4bfad
Daniel Pozzi <bonndan76@googlemail.com>
2013-05-19 19:07:27 +0200

    form factory

---
+++ src/Metagist/FormFactory.php
@@ -0,0 +1,86 @@
+<?php
+namespace Metagist;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Factory for forms.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class FormFactory
+{
+    /**
+     * the categories
+     * @var CategorySchema 
+     */
+    private $schema;
+   
+    /**
+     * symfony form factory
+     * @var Symfony\Component\Form\FormFactory 
+     */
+    private $formFactory;
+    
+    /**
+     * Constructor.
+     * 
+     * @param \Symfony\Component\Form\FormFactory $factory
+     * @param \Metagist\CategorySchema           $schema
+     */
+    public function __construct(\Symfony\Component\Form\FormFactory $factory, CategorySchema $schema)
+    {
+        $this->formFactory = $factory;
+        $this->schema      = $schema;
+    }
+    
+    /**
+     * Returns the rating form.
+     * 
+     * @return \Symfony\Component\Form\Form
+     */
+    public function getRateForm(array $versions = array(''))
+    {
+        $builder = $this->formFactory->createBuilder('form');
+
+        $form = $builder
+            ->add('rating', 'choice', array(
+                'choices' => range(1, 5),
+                'constraints' => new Assert\Range(array('min' => 1, 'max' => 5)),
+            ))
+            ->add('version', 'choice', array(
+                'choices' => array('') + $versions,
+                'multiple' => false,
+                'expanded' => false
+            ))
+            ->add('comment', 'textarea')
+            ->getForm()
+        ;
+        
+        return $form;
+    }
+    
+    /**
+     * Returns the form for metainfo contribution.
+     * 
+     * @param array $versions
+     * @return \Symfony\Component\Form\Form
+     */
+    public function getContributeForm(array $versions = array(''))
+    {
+        $builder = $this->formFactory->createBuilder('form');
+        $form = $builder
+            ->add('value', 'text', array(
+                'constraints' => new Assert\NotBlank(),
+            ))
+            ->add('version', 'choice', array(
+                'choices' => array('') + $versions,
+                'multiple' => false,
+                'expanded' => false
+            ))
+            ->getForm()
+        ;
+        
+        return $form;
+    }
+}
---
+++ tests/src/Metagist/FormFactoryTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace Metagist;
+
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Validator\Tests\Fixtures\FakeMetadataFactory;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+require_once __DIR__ . '/bootstrap.php';
+
+/**
+ * Tests the metagist form factory.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class FormFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * system under test
+     * @var FormFactory
+     */
+    private $factory;
+    
+    /**
+     * Test setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $metadata = new ClassMetadata('Symfony\Component\Form\Form');
+        $fakeFactory = new FakeMetadataFactory();
+        $fakeFactory->addMetadata($metadata);
+        
+        $validator = $this->getMock("\Symfony\Component\Validator\ValidatorInterface");
+        $validator->expects($this->once())
+            ->method('getMetadataFactory')
+            ->will($this->returnValue($fakeFactory));
+        
+        $symFactory = Forms::createFormFactoryBuilder()
+            ->addExtension(new \Symfony\Component\Form\Extension\Validator\ValidatorExtension($validator))
+            ->getFormFactory()
+            ;
+        $schema     = new CategorySchema(file_get_contents(__DIR__ . '/testdata/testcategories.json'));
+        $this->factory = new FormFactory($symFactory, $schema);
+    }
+    
+    /**
+     * Ensures an instance of \Symfony\Component\Form\Form is created and returned.
+     */
+    public function testGetContributeForm()
+    {
+        $versions = array('test');
+        $form = $this->factory->getContributeForm($versions);
+        $this->assertInstanceOf("\Symfony\Component\Form\Form", $form);
+    }
+    
+    /**
+     * Ensures an instance of \Symfony\Component\Form\Form is created and returned.
+     */
+    public function testGetRateForm()
+    {
+        $versions = array('test');
+        $form = $this->factory->getRateForm($versions);
+        $this->assertInstanceOf("\Symfony\Component\Form\Form", $form);
+    }
+}
