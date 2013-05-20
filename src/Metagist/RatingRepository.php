9af00a5455e269b2fa413cb43bd4841c5c7c9110
Daniel Pozzi <bonndan76@googlemail.com>
2013-05-20 07:07:39 +0200

    rating repo

---
+++ src/Metagist/RatingRepository.php
@@ -0,0 +1,124 @@
+<?php
+namespace Metagist;
+
+use \Doctrine\DBAL\Driver\Connection;
+use \Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Repository for package ratings.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class RatingRepository
+{
+    /**
+     * db connection
+     * @var \Doctrine\DBAL\Driver\Connection 
+     */
+    private $connection;
+    
+    /**
+     * Constructor.
+     * 
+     * @param \Doctrine\DBAL\Driver\Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+    
+    /**
+     * Retrieves all stored meta info for the given package.
+     * 
+     * @param \Metagist\Package $package
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function byPackage(Package $package)
+    {
+        $collection = new ArrayCollection();
+        $stmt = $this->connection->executeQuery(
+            'SELECT * FROM ratings WHERE package_id = ? ORDER BY time_updated DESC', array($package->getId())
+        );
+        while ($row = $stmt->fetch()) {
+            $collection->add(Rating::fromArray($row));
+        }
+        
+        return $collection;
+    }
+    
+    /**
+     * Saves (inserts) a single info.
+     * 
+     * @param \Metagist\Rating $rating
+     * @return int
+     */
+    public function save(Rating $rating)
+    {
+        $package = $rating->getPackage();
+        $userId  = $rating->getUserId();
+        
+        if ($package == null || $package->getId() == null || $userId == null) {
+            throw new \RuntimeException('Package ID and User ID must be set.');
+        }
+        $packageId = $package->getId();
+        
+        $this->connection->executeQuery(
+            'DELETE FROM ratings WHERE (package_id = ? AND user_id = ?)',
+            array($packageId, $userId)
+        );
+        
+        $stmt = $this->connection->executeQuery(
+            'INSERT INTO ratings (package_id, user_id, time_updated, version, rating, comment) 
+             VALUES (?, ?, ?, ?, ?, ?, ?)',
+            array(
+                $packageId,
+                $userId,
+                date('Y-m-d H:i:s', time()),
+                $rating->getVersion(),
+                $rating->getRating(),
+                $rating->getComment()
+            )
+        );
+        
+        return $stmt->rowCount();
+    }
+    
+    /**
+     * Retrieves metainfo that has been updated lately.
+     * 
+     * @param int $limit
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     * @todo parameter binding did not work.
+     */
+    public function best($limit = 25)
+    {
+        $stmt = $this->connection->executeQuery(
+            'SELECT AVG(r.rating) AS rating, r.package_id, p.identifier
+             FROM ratings r 
+             LEFT JOIN packages p ON p.id = r.package_id
+             GROUP BY r.package_id
+             ORDER BY reating DESC LIMIT ' . $limit,
+            array()
+        );
+        $collection = new ArrayCollection();
+        while ($row = $stmt->fetch()) {
+            $collection->add($this->createRatingWithDummyPackage($row));
+        }
+        return $collection;
+    }
+    
+    /**
+     * Creates a Rating instance with a dummy package based on the results
+     * of a joined query.
+     * 
+     * @param array $data
+     * @return Rating
+     */
+    private function createRatingWithDummyPackage(array $data)
+    {
+        $package = new Package($data['identifier'], $data['package_id']);
+        $data['package'] = $package;
+        $rating = Rating::fromArray($data);
+        return $rating;
+    }
+}
---
+++ tests/src/Metagist/RatingRepositoryTest.php
@@ -0,0 +1,168 @@
+<?php
+namespace Metagist;
+
+require_once __DIR__ . '/bootstrap.php';
+
+/**
+ * Tests the rating repo class.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class RatingRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * system under test
+     * @var RatingRepository
+     */
+    private $repo;
+    
+    /**
+     * connection mock
+     * @var \Doctrine\DBAL\Connection 
+     */
+    private $connection;
+    
+    /**
+     * Test setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->connection = $this->getMockBuilder("\Doctrine\DBAL\Connection")
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->repo = new RatingRepository($this->connection);
+    }
+    
+    /**
+     * Ensures the params are validated.
+     */
+    public function testByPackage()
+    {
+        $statement = $this->createMockStatement();
+        $statement->expects($this->at(0))
+            ->method('fetch')
+            ->will($this->returnValue(
+                array(
+                    'category' => 'cat123',
+                    'group' => 'group123',
+                    'value' => 'val123'))
+            );
+        $statement->expects($this->at(1))
+            ->method('fetch')
+            ->will($this->returnValue(false));
+        
+        $this->connection->expects($this->once())
+            ->method('executeQuery')
+            ->will($this->returnValue($statement));
+        
+        $package = new Package('test/test123', 123);
+        $collection = $this->repo->byPackage($package);
+        $this->assertInstanceOf("\Doctrine\Common\Collections\Collection", $collection);
+        $info = $collection->get(0);
+        $this->assertInstanceOf("\Metagist\Rating", $info);
+    }
+    
+    /**
+     * Ensures a package can be saved.
+     */
+    public function testSave()
+    {
+        $statement = $this->createMockStatement();
+        $this->connection->expects($this->at(0))
+            ->method('executeQuery')
+            ->with($this->stringContains('DELETE FROM ratings'));
+        $this->connection->expects($this->at(1))
+            ->method('executeQuery')
+            ->with($this->stringContains('INSERT INTO ratings'))
+            ->will($this->returnValue($statement));
+        
+        $package = new Package('test/test123', 123);
+        $rating = Rating::fromArray(array(
+            'package' => $package,
+            'user_id' => 13
+        ));
+        $this->repo->save($rating);
+    }
+    
+    /**
+     * Ensures a package cannot be saved without user_id.
+     */
+    public function testSaveNoUserIdException()
+    {
+        $package = new Package('test/test123', 123);
+        $rating = Rating::fromArray(array(
+            'package' => $package,
+        ));
+        $this->setExpectedException("\RuntimeException");
+        $this->repo->save($rating);
+    }
+    
+    /**
+     * Ensures a package cannot be saved without a package.
+     */
+    public function testSaveNoPackageException()
+    {
+        $rating = Rating::fromArray(array(
+            'user_id' => 13,
+        ));
+        $this->setExpectedException("\RuntimeException");
+        $this->repo->save($rating);
+    }
+    
+    /**
+     * Ensures a package cannot be saved without a package.
+     */
+    public function testSaveNoPackageIdException()
+    {
+        $package = new Package('test/test123');
+        $rating = Rating::fromArray(array(
+            'user_id' => 13,
+            'package' => $package,
+        ));
+        $this->setExpectedException("\RuntimeException");
+        $this->repo->save($rating);
+    }
+    
+    /**
+     * Ensures a package is returned if found.
+     */
+    public function testBest()
+    {
+        $data = array(
+            'id' => 1,
+            'identifier' => 'test/test',
+            'description' => 'test',
+            'versions' => 'dev-master',
+            'package_id' => 1,
+        );
+        $statement = $this->createMockStatement();
+        $statement->expects($this->at(0))
+            ->method('fetch')
+            ->will($this->returnValue($data));
+        $statement->expects($this->at(1))
+            ->method('fetch')
+            ->will($this->returnValue(false));
+        
+        $this->connection->expects($this->once())
+            ->method('executeQuery')
+            ->will($this->returnValue($statement));
+        
+        $collection = $this->repo->best();
+        $this->assertInstanceOf("\Doctrine\Common\Collections\ArrayCollection", $collection);
+        $this->assertEquals(1, count($collection));
+        $rating = $collection->get(0);
+        $this->assertInstanceOf("\Metagist\Rating", $rating);
+    }
+    
+    /**
+     * Creates a statement mock, the provided HydratorMockStatement seems to be broken.
+     * 
+     * @param array $methods
+     * @return Statement mock
+     */
+    protected function createMockStatement(array $methods = array('rowCount', 'fetch'))
+    {
+        return $this->getMock('stdClass', $methods);
+    }
+}
