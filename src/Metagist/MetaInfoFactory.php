0d1b434b1941984e9b80515c5e006340d9ce0116
Daniel Pozzi <bonndan76@googlemail.com>
2013-05-31 19:20:50 +0200

    metainfo factory collects contributors and commits from github

--- src/Metagist/MetaInfoFactory.php
+++ src/Metagist/MetaInfoFactory.php
@@ -12,6 +12,13 @@ use \Doctrine\Common\Collections\ArrayCollection;
 class MetaInfoFactory
 {
     /**
+     * Client to query the github api.
+     * 
+     * @var \Github\Client 
+     */
+    protected $githubClient;
+    
+    /**
      * Creates metainfos based on a packagist package object.
      * 
      * @param \Packagist\Api\Result\Package $package
@@ -26,9 +33,10 @@ class MetaInfoFactory
         
         if ($firstVersion != false) {
             $versionString = $firstVersion->getVersion();
+            $repository    = $package->getRepository();
             $metainfos = new ArrayCollection(
                 array(
-                    MetaInfo::fromValue(CategorySchema::REPOSITORY_IDENTIFIER, $package->getRepository(), $versionString),
+                    MetaInfo::fromValue(CategorySchema::REPOSITORY_IDENTIFIER, $repository, $versionString),
                     MetaInfo::fromValue('community/homepage', $firstVersion->getHomepage(), $versionString),
                     MetaInfo::fromValue('reliability/maintainers', count($package->getMaintainers()), $versionString),
                     MetaInfo::fromValue('reliability/requires', count($firstVersion->getRequire()), $versionString),
@@ -40,8 +48,66 @@ class MetaInfoFactory
             if (is_array($licenses)) {
                 $metainfos->add(MetaInfo::fromValue('documentation/license', implode(' ', $licenses), $versionString));
             }
+            
+            //add additional infos from github
+            if ($repository != null) {
+                $githubMetainfos = $this->fromGithubRepo($repository);
+                foreach ($githubMetainfos as $metainfo) {
+                    /* @var $metainfo MetaInfo */
+                    $metainfo->setVersion($versionString);
+                    $metainfos->add($metainfo);
+                }
+            }
         }
         
         return $metainfos;
+    }
+    
+    /**
+     * Setter to inject a github client for fetching infos from github.
+     * 
+     * @param \Github\Client $client
+     */
+    public function setGitHubClient(\Github\Client $client)
+    {
+        $this->githubClient = $client;
+    }
+    
+    /**
+     * Creates metainfos by retrieving repository data from github.
+     * 
+     * @param string $url repo url
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function fromGithubRepo($url)
+    {
+        if ($this->githubClient === null) {
+            throw new \RuntimeException('No github client injected.');
+        }
+        
+        $needle = '://github.com/';
+        if (strpos($url, $needle) === FALSE) {
+            return null;
+        }
+        $parts  =  parse_url($url); 
+        @list ($owner, $repo) = explode('/', ltrim($parts['path'], '/'));
+        if ($owner == '' || $repo == '') {
+            return null;
+        }
+        $owner = strtolower(ltrim($owner, '/'));
+        $repo  = strtolower(basename($repo, '.git'));
+        
+        $collection = new ArrayCollection(array());
+        
+        $stats = new \Metagist\GithubApi\Stats($this->githubClient);
+        $contributors = $stats->contributors($owner, $repo);
+        $collection->add(MetaInfo::fromValue('reliability/contributors', count($contributors)));
+        $commitCount = 0;
+        foreach ($contributors as $contributor) {
+            $commitCount += $contributor['total'];
+        }
+        $collection->add(MetaInfo::fromValue('reliability/commits', $commitCount));
+        
+        return $collection;
     }
 }
--- tests/src/Metagist/MetaInfoFactoryTest.php
+++ tests/src/Metagist/MetaInfoFactoryTest.php
@@ -63,4 +63,76 @@ class MetaInfoFactoryTest extends \PHPUnit_Framework_TestCase
         $collection = $this->factory->fromPackagistPackage($package);
         $this->assertEmpty($collection);
     }
+    
+    /**
+     * Ensures the github client can be injected.
+     */
+    public function testInjectGithubClient()
+    {
+       $client = $this->getMockBuilder("\Github\Client")
+           ->disableOriginalConstructor()
+           ->getMock();
+       $this->factory->setGitHubClient($client);
+       $this->assertAttributeEquals($client, 'githubClient', $this->factory);
+    }
+    
+    /**
+     * Ensures that the client must be injected first.
+     */
+    public function testFromGithubRepoThrowsNoClientException()
+    {
+        $this->setExpectedException("\RuntimeException");
+        $this->factory->fromGithubRepo('http://an.url');
+    }
+    
+    /**
+     * Ensures that only github.com urls are parsed.
+     */
+    public function testFromGithubRepoReturnsNullIfUrlNotGithub()
+    {
+        $client = $this->getMockBuilder("\Github\Client")
+           ->disableOriginalConstructor()
+           ->getMock();
+        $this->factory->setGitHubClient($client);
+        $result = $this->factory->fromGithubRepo('http://an.url');
+        $this->assertNull($result);
+    }
+    
+    /**
+     * Ensures that only urls with path are parsed.
+     */
+    public function testFromGithubRepoReturnsNullIfNoPath()
+    {
+        $client = $this->getMockBuilder("\Github\Client")
+           ->disableOriginalConstructor()
+           ->getMock();
+        $this->factory->setGitHubClient($client);
+        $result = $this->factory->fromGithubRepo("https://github.com/");
+        $this->assertNull($result);
+    }
+    
+    /**
+     * Ensures that only urls with path are parsed.
+     */
+    public function testFromGithubRepoCollectsContributorsAndCommits()
+    {
+        $client = $this->getMockBuilder("\Github\Client")
+           ->disableOriginalConstructor()
+           ->getMock();
+        $response = $this->getMock("\Github\HttpClient\Message\Response");
+        $response->expects($this->any())
+            ->method('getContent')
+            ->will($this->returnValue(array()));
+        $httpClient = $this->getMock("\Github\HttpClient\HttpClientInterface");
+        $httpClient->expects($this->any())
+            ->method('get')
+            ->will($this->returnValue($response));
+        $client->expects($this->any())
+            ->method('getHttpClient')
+            ->will($this->returnValue($httpClient));
+        
+        $this->factory->setGitHubClient($client);
+        $result = $this->factory->fromGithubRepo("https://github.com/owner/repo");
+        $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $result);
+    }
 }
