7afc955549e3b68fecdb85addca2d3794a706e71
Daniel Pozzi <bonndan76@googlemail.com>
2013-06-25 21:34:38 +0200

    Improved RMT usage

--- src/VersionStampAction.php
+++ src/VersionStampAction.php
@@ -29,7 +29,7 @@ class VersionStampAction extends BaseAction
             array('file' => 'version.php'), $options
         );
     }
-
+    
     /**
      * Writes the current version to the target file.
      * 
@@ -37,10 +37,18 @@ class VersionStampAction extends BaseAction
      */
     public function execute()
     {
-        $version = Context::get('version-persister')->getCurrentVersionTag();
+        $version = Context::getInstance()->getParameter('new-version');
+        if ($version == null) {
+            throw new \RuntimeException('Could not determine the new version.');
+        }
+        
         $template = "<?php\ndefine('METAGIST_VERSION', '%s');\n";
 
         file_put_contents($this->options['file'], sprintf($template, $version));
+        
+        exec('git commit version.php --allow-empty -m "Updated version.php file"', $result, $exitCode);
+        if ($exitCode != 0) {
+            throw new \RuntimeException('Could not commit the version file: ' . implode(' ', $result));
+        }
     }
-
 }
