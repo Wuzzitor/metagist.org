1e3cbf14bdf94f81c4affe94825e02f78533870b
Daniel Pozzi <bonndan76@googlemail.com>
2013-07-16 22:09:00 +0200

    assetic fix

--- config/common.php
+++ config/common.php
@@ -19,14 +19,14 @@ $app['twig.options.cache'] = $app['cache.path'] . '/twig';
 
 // Assetic
 $app['assetic.enabled']              = true;
-$app['assetic.path_to_cache']        = $app['cache.path'] . '/assetic' ;
+$app['assetic.path_to_cache']      = $app['cache.path'] . '/assetic' ;
 $app['assetic.path_to_web']          = __DIR__ . '/../web/assets';
 $app['assetic.input.path_to_assets'] = __DIR__ . '/../resources/assets';
 
 $app['assetic.input.path_to_css']       = $app['assetic.input.path_to_assets'] . '/less/style.less';
 $app['assetic.output.path_to_css']      = 'css/styles.css';
 $app['assetic.input.path_to_js']        = array(
-    __DIR__.'/../../vendor/twitter/bootstrap/js/*.js',
+    __DIR__.'/../vendor/twitter/bootstrap/js/*.js',
     $app['assetic.input.path_to_assets'] . '/js/script.js',
 );
 $app['assetic.output.path_to_js']       = 'js/scripts.js';
